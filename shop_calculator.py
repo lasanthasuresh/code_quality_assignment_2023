4028e7c1d6a483162fdc7998addde2bd18454033
Dasundh99 <119101008+Dasundh99@users.noreply.github.com>
2023-09-22 11:02:18 +0000

    code has edited according to the coding standards

--- shop_calculator.py
+++ shop_calculator.py
@@ -1,95 +1,110 @@
+#Hettiarachchi UADD
+#EG_2020_3964
+
+#this is a calculator for a shop
 import csv
 
-discnts = {}
+
+#defining of variables
+discounts = {}
 taxes = {}
-prmtns = {}
-curr_rates = {}
-invntry = {}
+promotions = {}
+current_rates = {}
+inventry = {}
 
-def ld_discnts(file):
-    with open(file) as dscnt_file:
-        r = csv.reader(dscnt_file)
+
+#functions to calculate discount
+def ld_discounts(file):
+    with open(file) as discount_file:
+        r = csv.reader(discount_file)
         for rw in r:
-            discnts[rw[0].upper()] = float(rw[1])
+            discounts[rw[0].upper()] = float(rw[1])
 
+#function to calculate taxes
 def ld_taxes(file):
     with open(file) as tax_file:
         r = csv.reader(tax_file)
         for rw in r:
             taxes[rw[0].upper()] = float(rw[1])
 
-def ld_prmtns(file):
-    with open(file) as prmtn_file:
-        r = csv.reader(prmtn_file)
-        for rw in r:
-            prmtns[rw[0]] = float(rw[1])
 
-def ld_curr_rates(file):
+#function to calculate promotions
+def ld_promotions(file):
+    with open(file) as promotion_file:
+        r = csv.reader(promotion_file)
+        for rw in r:
+            promotions[rw[0]] = float(rw[1])
+
+
+#function to calculate current rates
+def ld_current_rates(file):
     with open(file) as currency_file:
         r = csv.reader(currency_file)
         for rw in r:
-            curr_rates[rw[0].upper()] = float(rw[1])
+            current_rates[rw[0].upper()] = float(rw[1])
 
-def ld_invntry(file):
-    with open(file) as inv_file:
-        r = csv.reader(inv_file)
+#function to calculate inventries
+def ld_inventry(file):
+    with open(file) as inventry_file:
+        r = csv.reader(inventry_file)
         for rw in r:
-            invntry[rw[0].upper()] = int(rw[1])
+            inventry[rw[0].upper()] = int(rw[1])
 
+#defining the main function
 def main():
 
-    ld_discnts('data/discounts.csv')
+    ld_discounts('data/discounts.csv')
     ld_taxes('data/tax_rates.csv')
-    ld_prmtns('data/promotions.csv')
-    ld_curr_rates('data/currency_rates.csv')
-    ld_invntry('data/inventory.csv')
+    ld_promotions('data/promotions.csv')
+    ld_current_rates('data/currency_rates.csv')
+    ld_inventry('data/inventory.csv')
 
     with open('shopping_cart.csv') as file:
         cart = {rw[0]: int(rw[1]) for rw in csv.reader(file)}
 
     total_price_usd = 0
 
-    for itm, qty in cart.items():
+    for item, quantity in cart.items():
 
-        itm = itm.upper()
+        item = item.upper()
 
-        if invntry.get(itm, 0) < qty:
-            print(f"Cannot proceed, insufficient inventory for {itm}")
+        if inventry.get(item, 0) < quantity:
+            print(f"Cannot proceed, insufficient inventory for {item}")
             return
 
-        promo = prmtns.get(itm)
+        promo = promotions.get(item)
         if promo:
-            qty = qty - (qty // promo)
-        dscnt = discnts.get(itm, 0)
+            quantity = quantity - (quantity // promo)
+        discount = discounts.get(item, 0)
         price = 1.0  
-        dscntd_price = price - (price * dscnt)
-        item_price = dscntd_price * qty
+        discounted_price = price - (price * discount)
+        item_price = discounted_price * quantity
         total_price_usd += item_price
-        invntry[itm] -= qty 
+        inventry[item] -= quantity 
 
     available_states = list(taxes.keys())
     print("Enter your state. Available states are: ", ', '.join(available_states))
     state = input()
 
     tax_rate = taxes.get(state, 0)
-    tax_amt = total_price_usd * tax_rate
+    tax_amount = total_price_usd * tax_rate
 
-    final_price_usd = total_price_usd + tax_amt
+    final_price_usd = total_price_usd + tax_amount
 
     print("-------- SHOPPING CART SUMMARY --------")
     print("Item   | Qty | Price")
     print("-----------------------------")
-    for itm, qty in cart.items():
-        print(f"{itm.ljust(6)} | {str(qty).ljust(3)} | USD {dscntd_price * qty:.2f}")
+    for item, quantity in cart.items():
+        print(f"{item.ljust(6)} | {str(quantity).ljust(3)} | USD {discounted_price * quantity:.2f}")
     print("-----------------------------")
     print(f"Subtotal: USD {total_price_usd:.2f}")
-    print(f"Tax ({state}): USD {tax_amt:.2f}")
+    print(f"Tax ({state}): USD {tax_amount:.2f}")
     print(f"Total: USD {final_price_usd:.2f}")
 
-    available_currencies = list(curr_rates.keys())
+    available_currencies = list(current_rates.keys())
     selected_currency = input("Select currency for payment ({}): ".format(', '.join(available_currencies)))
 
-    final_price_currency = final_price_usd * curr_rates.get(selected_currency)
+    final_price_currency = final_price_usd * current_rates.get(selected_currency)
 
     print(f"Total in {selected_currency}: {final_price_currency:.2f}")
 
