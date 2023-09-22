377a01d55c7713ac497bc45afd5daa908c7d8c63
Tharukshi Diyunugala <139604971+TharukshiDiyunugala@users.noreply.github.com>
2023-09-22 10:59:39 +0000

    ENG3918 answers

--- TravelCostCalculator.java
+++ TravelCostCalculator.java
@@ -1,3 +1,4 @@
+//EG/2020/3918 Diyunugala D.T.N.
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,10 +11,12 @@ public class TravelCostCalculator {
     static Map<String, Double> b = new HashMap<>();
     static Map<String, Double> c = new HashMap<>();
 
-    static void l1(String file) throws IOException {
+    static void l1(String file) throws IOException
+    {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String i; 
-        while ((i = reader.readLine()) != null) {
+        while ((i = reader.readLine()) != null)
+        {
             String[] p = i.split(",");
             a.put(p[0].toUpperCase(), Double.parseDouble(p[1]));
         }
@@ -22,22 +25,26 @@ public class TravelCostCalculator {
     static void l2(String file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String i;
-        while ((i = reader.readLine()) != null) {
+        while ((i = reader.readLine()) != null)
+        {
             String[] p = i.split(",");
             b.put(p[0].toUpperCase(), Double.parseDouble(p[1]));
         }
     }
 
-    static void l3(String file) throws IOException {
+    static void l3(String file) throws IOException
+    {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String i;
-        while ((i = reader.readLine()) != null) {
+        while ((i = reader.readLine()) != null)
+        {
             String[] p = i.split(",");
             c.put(p[0].toUpperCase(), Double.parseDouble(p[1]));
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         try {
             l1("data/hotel_rates.csv");
             l2("data/exchange_rates.csv");
@@ -48,14 +55,14 @@ public class TravelCostCalculator {
             System.out.print("Enter your destination: ");
             String destination = reader.readLine().toUpperCase();
 
-            double flight_cost = c.getOrDefault(destination, 0.0);
-            double hotel_cost = a.getOrDefault(destination, 0.0);
+            double FLIGHT_COST = c.getOrDefault(destination, 0.0);
+            double HOTEL_COST = a.getOrDefault(destination, 0.0);
 
             System.out.print("Enter your stay duration in days: ");
-            int stay_duration = Integer.parseInt(reader.readLine());
-            hotel_cost *= stay_duration;
+            int STAY_DURATION = Integer.parseInt(reader.readLine());
+            hotel_cost *= STAY_DURATION;
 
-            double total_cost_usd = flight_cost + hotel_cost;
+            double total_cost_usd = FLIGHT_COST + HOTEL_COST;
 
             System.out.printf("Flight cost: USD %.2f\n", flight_cost);
             System.out.printf("Hotel cost (%d days): USD %.2f\n", stay_duration, hotel_cost);
@@ -68,7 +75,9 @@ public class TravelCostCalculator {
             double final_price_local_currency = total_cost_usd * b.get(selected_currency);
 
             System.out.printf("Total in %s: %.2f\n", selected_currency, final_price_local_currency);
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
