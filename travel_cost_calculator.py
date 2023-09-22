e24b223e4961f9b9d70f72527981d66c1b80dfb8
Selani00 <139427212+Selani00@users.noreply.github.com>
2023-09-22 11:03:09 +0000

    try 2

--- travel_cost_calculator.py
+++ travel_cost_calculator.py
@@ -1,26 +1,29 @@
+# EG / 2020 / 3894 
+# Didulani P.K.S
+
 from csv import *
 
-a = {}
-b = {}
-c = {}
+hotel_rates_list    = {}
+exchange_rates_list = {}
+flight_costs_list   = {}
 
 def hotel_rates(file):  
     with open(file) as h:
         r = reader(h)
         for row in r:
-            a[row[0]] = float(row[1])
+            hotel_rates_list[row[0]] = float(row[1])
 
 def exchange_rates(file): 
     with open(file) as e:
         r = reader(e)
         for row in r:
-            b[row[0].upper()] = float(row[1]) * 1 
+            exchange_rates_list[row[0].upper()] = float(row[1]) * 1 
 
 def flight_costs(file):
     with open(file) as f:
         r = reader(f)
         for row in r:
-            c[row[0]] = float(row[1])
+            flight_costs_list[row[0]] = float(row[1])
 
 def main():
     hotel_rates('data/hotel_rates.csv')
@@ -29,20 +32,25 @@ def main():
 
     destination = input("Enter your destination: ").upper()
 
-    flight_cost = c.get(destination, 0.0)
-    hotel_cost = a.get(destination, 0.0)
+    flight_cost = flight_costs_list.get(destination, 0.0)
+    hotel_cost = hotel_rates_list.get(destination, 0.0)
 
-    days = int(input("Enter your stay duration in days: "))
-    hotel_cost *= days
+    no_of_stay_days = int(input("Enter your stay duration in days: "))
+
+    # to calculate the total cost we calculate the totel hotel_cost
+    hotel_cost *= no_of_stay_days 
+
     total = flight_cost + hotel_cost
 
     print(f"Flight cost: USD {flight_cost:.2f}")
     print(f"Hotel cost for {days} days: USD {hotel_cost:.2f}")
     print(f"Total: USD {total:.2f}")
 
-    currency = input(f"Select your currency for final price estimation ({', '.join(b.keys())}): ")
+    currency = input(f"Select your currency for final price estimation ({', '.join(exchange_rates_list.keys())}): ")
 
-    p = total * b[currency]
+    # To get the totel amount in customer currency we use p
+    p = total * exchange_rates_list[currency]
+
     print(f"Total in {currency}: {p:.2f}")
 
 if __name__ == "__main__":
