5751b3373b281165bf65f60169731d6be9073fd7
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-09-22 11:01:56 +0000

    Add neccessary comments

--- TravelCostCalculator.java
+++ TravelCostCalculator.java
@@ -7,10 +7,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TravelCostCalculator {
+    //declare hashmaps as files read
     static Map<String, Double> hotelRates = new HashMap<>();
     static Map<String, Double> exchangeRates = new HashMap<>();
     static Map<String, Double> flightCosts = new HashMap<>();
 
+    //Read cvs file and assign into hash map  
     static void readCsvFile(String file, HashMap<String, Double> hashMap) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line; 
@@ -22,22 +24,26 @@ public class TravelCostCalculator {
 
     public static void main(String[] args) {
         try {
+            //Read csv and assign into hash maps 
             readCsvFile("data/hotel_rates.csv", hotelRates);
             readCsvFile("data/exchange_rates.csv", exchangeRates);
             readCsvFile("data/flight_costs.csv", flightCosts);
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
 
             System.out.print("Enter your destination: ");
-            String destination = reader.readLine().toUpperCase();
+            String destination = userInputReader.readLine().toUpperCase();
 
             double flightCost = flightCosts.getOrDefault(destination, 0.0);
             double hotelCost = hotelRates.getOrDefault(destination, 0.0);
 
             System.out.print("Enter your stay duration in days: ");
-            int stayDuration = Integer.parseInt(reader.readLine());
+            int stayDuration = Integer.parseInt(userInputReader.readLine());
+
+            //Total hotel cost should propotional with days of stay.
             hotelCost *= stayDuration;
 
+            //Total cost include both flight and hotel cost
             double totalCostUSD = flightCost + hotelCost;
 
             System.out.printf("Flight cost: USD %.2f\n", flightCost);
@@ -46,8 +52,9 @@ public class TravelCostCalculator {
 
             String[] availableCurrencies = exchangeRates.keySet().toArray(new String[0]);
             System.out.print("Select your currency for final price estimation(" + String.join(", ", availableCurrencies) + "): ");
-            String selectedCurrency = reader.readLine();
+            String selectedCurrency = userInputReader.readLine();
 
+            //Value of the local currency should update with exchange rate.
             double finalPriceLocalCurrency = totalCostUSD * exchangeRates.get(selectedCurrency);
 
             System.out.printf("Total in %s: %.2f\n", selectedCurrency, finalPriceLocalCurrency);
